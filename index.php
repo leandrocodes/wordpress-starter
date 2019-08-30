c21646cbe025dd1db9d822672d901bc43b820a91
leandrocodes <leaxviana140@gmail.com>
2019-08-30 16:17:16 -0400

    starting the course

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<?php
-    get_header();
-?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<?php wp_head(); ?>
+</head>
+<body>
 
-<h1> This is my Index</h1>
-
-<?php
-    get_footer();
-?>
+</body>
+</html>

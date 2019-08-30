da79b28ac8de83238d9973032bf1f9b734be6755
leandrocodes <leaxviana140@gmail.com>
2019-08-30 16:44:36 -0400

    verifyin posts

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<title><?php bloginfo('name'); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	<?php wp_head(); ?>
 </head>
+
 <body>
 	<header>
 		<h1><?php bloginfo('name'); ?></h1>
 		<h3><?php bloginfo('description'); ?></h3>
 	</header>
+
+	<main>
+		<?php if (have_posts()) : ?>
+			post found
+		<?php else :
+			echo wpautop('No posts yet...');
+		endif;
+		?>
+	</main>
+
 </body>
+
 </html>
